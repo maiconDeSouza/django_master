6ea964d4c4208bbcd324d3faa09f7a44e522f2dd
Maicon Souza <mcn@dev.Home>
2024-08-26 06:26:12 -0300

    26-08-24 06:26

--- 06_criando_e_conhecendo_seu_projeto/cars/views.py
+++ 06_criando_e_conhecendo_seu_projeto/cars/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_cars(request):
+    return HttpResponse('Bem Vindo a nossa agencia de carros')
