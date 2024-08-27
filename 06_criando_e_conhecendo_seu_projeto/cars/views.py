8568c527cb2f364feaa0f599acd690b7d310690e
Maixon Souza <maicon_souza09@hotmail.com>
2024-08-27 07:47:28 -0300

    27-08-24 07:47

--- 06_criando_e_conhecendo_seu_projeto/cars/views.py
+++ 06_criando_e_conhecendo_seu_projeto/cars/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_cars(request):
-    return HttpResponse('Bem Vindo a nossa agencia de carros')
+    return render(request, 'cars/index.html', {"cars": {"model": "Astra"}})
