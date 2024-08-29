de0ed9a90b1c32a5393c2fbfa5d1197a4b042a78
Maicon Souza <maicon_souza09@hotmail.com>
2024-08-29 08:22:45 -0300

    29-08-24 08:22

--- 06_criando_e_conhecendo_seu_projeto/cars/views.py
+++ 06_criando_e_conhecendo_seu_projeto/cars/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.forms import Carform
 
 
-def home_cars(request):
+def home_cars_view(request):
     model = request.GET.get("search")
     if model:
         cars = Car.objects.filter(model__icontains=model).order_by('-pk')
@@ -10,3 +11,8 @@ def home_cars(request):
     else:
         cars = Car.objects.all().order_by('-pk')
         return render(request, 'cars/index.html', {"cars": cars})
+
+
+def new_car_view(request):
+    new_car_form = Carform()
+    return render(request, 'cars/new_car.html', {'new_car_form': new_car_form})
