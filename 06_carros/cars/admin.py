5c3400577e9136bb4099020dc450dedd4a63695f
Maixon Souza <maicon_souza09@hotmail.com>
2024-09-20 05:51:58 -0300

    20-09-24 05:51

--- 06_carros/cars/admin.py
+++ 06_carros/cars/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Car
 
 # Register your models here.
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
-    search_fields = ('model',)
+    search_fields = ('model', 'brand')
+
 
 admin.site.register(Car, CarAdmin)
--- 06_carros/cars/models.py
+++ 06_carros/cars/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
     model = models.CharField(max_length=200)
@@ -8,3 +10,6 @@ class Car(models.Model):
     factory_year = models.IntegerField(blank=True, null=True)
     model_year = models.IntegerField(blank=True, null=True)
     value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.model
