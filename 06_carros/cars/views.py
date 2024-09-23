16b1d7639b8b42604c9d8cbe80c8c598d25fd79b
Maixon Souza <maicon_souza09@hotmail.com>
2024-09-23 06:49:41 -0300

    23-09-24 06:49

--- 06_carros/cars/views.py
+++ 06_carros/cars/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cars_view(request):
+    html = '''
+    <html>
+        <head>
+            <title>Meus carros</title>
+        </head>
+        <body>
+            <h1> Carros PycodeBR</h1>
+            <h3>Só Carro Top!</h3>
+        </body>
+    </html>
+'''
+    return HttpResponse(html)
--- 06_carros/setup/urls.py
+++ 06_carros/setup/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars import views as cars_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_views.cars_view)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
