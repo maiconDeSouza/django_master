94dd19a12af619c46b933105ca3f7914a5e954cf
Maixon Souza <maicon_souza09@hotmail.com>
2024-09-30 06:57:03 -0300

    30-09-24 06:57

--- 06_carros/cars/views.py
+++ 06_carros/cars/views.py
@@ -1,17 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def cars_view(request):
-    html = '''
-    <html>
-        <head>
-            <title>Meus carros</title>
-        </head>
-        <body>
-            <h1> Carros PycodeBR</h1>
-            <h3>Só Carro Top!</h3>
-        </body>
-    </html>
-'''
-    return HttpResponse(html)
+    context = {
+        "cars": {
+            "model": "Astra 2.0"
+        }
+    }
+    return render(request, 'cars.html', context)
