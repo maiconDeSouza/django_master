1776fc651a5940886ad631d7374b194da0ce5141
Maicon Souza <maicon_souza09@hotmail.com>
2024-10-05 19:58:51 -0300

    05-10-24 19:58

--- 06_carros/cars/views.py
+++ 06_carros/cars/views.py
@@ -1,12 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .form import CarForm
 
 
 def cars_view(request):
-    cars = Car.objects.all()
-    print(request.GET.get('search'))
-    print(request.GET.get('name'))
+    cars = {}
+    search = request.GET.get('search')
+
+    if search:
+        cars = Car.objects.filter(model__icontains=search).order_by('model')
+    else:
+        cars = Car.objects.all().order_by('model')
+
     context = {
         "cars": cars
     }
     return render(request, 'cars.html', context)
+
+
+def new_car_view(requets):
+    new_car_form = CarForm()
+    if requets.method == 'POST':
+        new_car_form = CarForm(requets.POST, requets.FILES)
+
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+
+    context = {
+        "new_car_form": new_car_form
+    }
+
+    return render(requets, 'new_car.html', context)
