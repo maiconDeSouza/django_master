168a9a9b18618b60cdb6e5337087863375881cdb
Maixon Souza <maicon_souza09@hotmail.com>
2024-10-07 20:17:05 -0300

    07-10-24 20:17

--- 06_carros/accounts/views.py
+++ 06_carros/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 
 def register_view(request):
@@ -7,7 +8,7 @@ def register_view(request):
         user_creation_form = UserCreationForm(request.POST)
         if user_creation_form.is_valid():
             user_creation_form.save()
-            return redirect('car_list')
+            return redirect('login')
     else:
         # Exibe o formulário vazio para preenchimento
         user_creation_form = UserCreationForm()
@@ -16,3 +17,31 @@ def register_view(request):
         'user_creation_form': user_creation_form
     }
     return render(request, 'register.html', context)
+
+
+def login_view(request):
+    if request.method == 'GET':
+        login_form = AuthenticationForm()
+        context = {
+            "login_form": login_form
+        }
+        return render(request, 'login.html', context)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+            context = {
+                "login_form": login_form
+            }
+            return render(request, 'login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
