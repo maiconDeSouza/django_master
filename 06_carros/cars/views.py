21adf04e6bdf9ef7db9b7d55a0864cadc28a3f0a
Maicon Souza <maicon_souza09@hotmail.com>
2024-10-08 17:05:05 -0300

    08-10-24 17:05

--- 06_carros/cars/views.py
+++ 06_carros/cars/views.py
@@ -1,7 +1,13 @@
+from typing import Any
+# from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from .models import Car
 from .form import CarModelForm
-from django.views import View
+from django.urls import reverse_lazy
+# from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 # def cars_view(request):
@@ -18,21 +24,33 @@ from django.views import View
 #     }
 #     return render(request, 'cars.html', context)
 
-class CarsView(View):
-    def get(self, request):
-        cars = {}
-        search = request.GET.get('search')
+# class CarsView(View):
+#     def get(self, request):
+#         cars = {}
+#         search = request.GET.get('search')
 
+#         if search:
+#             cars = Car.objects.filter(
+#                 model__icontains=search).order_by('model')
+#         else:
+#             cars = Car.objects.all().order_by('model')
+
+#         context = {
+#             "cars": cars
+#         }
+#         return render(request, 'cars.html', context)
+
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
+
+    def get_queryset(self):
+        search = self.request.GET.get('search')
         if search:
-            cars = Car.objects.filter(
-                model__icontains=search).order_by('model')
+            return super().get_queryset().filter(model__icontains=search).order_by('model')
         else:
-            cars = Car.objects.all().order_by('model')
-
-        context = {
-            "cars": cars
-        }
-        return render(request, 'cars.html', context)
+            return super().get_queryset().order_by('model')
 
 
 # def new_car_view(requets):
@@ -51,22 +69,51 @@ class CarsView(View):
 #     return render(requets, 'new_car.html', context)
 
 
-class NewCarView(View):
-    def get(self, request):
-        new_car_from = CarModelForm()
-        context = {
-            "new_car_form": new_car_from
-        }
-        return render(request, 'new_car.html', context)
+# class NewCarView(View):
+#     def get(self, request):
+#         new_car_from = CarModelForm()
+#         context = {
+#             "new_car_form": new_car_from
+#         }
+#         return render(request, 'new_car.html', context)
 
-    def post(self, request):
-        new_car_from = CarModelForm(request.POST, request.FILES)
-        if new_car_from.is_valid():
-            new_car_from.save()
-            return redirect('cars_list')
-        else:
-            new_car_form = CarModelForm()
-            context = {
-                "new_car_form": new_car_form
-            }
-            return render(request, 'new_car.html', context)
+#     def post(self, request):
+#         new_car_from = CarModelForm(request.POST, request.FILES)
+#         if new_car_from.is_valid():
+#             new_car_from.save()
+#             return redirect('cars_list')
+#         else:
+#             new_car_form = CarModelForm()
+#             context = {
+#                 "new_car_form": new_car_form
+#             }
+#             return render(request, 'new_car.html', context)
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarUpdatelView(UpdateView):
+    model = Car
+    template_name = 'car_update.html'
+    form_class = CarModelForm
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={"pk": self.object.pk})
+
+
+@method_decorator(login_required(login_url='login'), name='dispatch')
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
--- 06_carros/setup/urls.py
+++ 06_carros/setup/urls.py
@@ -18,13 +18,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewCarView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdatelView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
-    path('cars/', CarsView.as_view(), name='cars_list'),
-    path('new_car/', NewCarView.as_view(), name='new_car'),
+    path('cars/', CarsListView.as_view(), name='cars_list'),
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
+    path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+    path('car/<int:pk>/update/', CarUpdatelView.as_view(), name='car_update'),
+    path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
