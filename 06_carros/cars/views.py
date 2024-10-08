602cc4bcac369c37e71ec4012bee1d33042db2be
Maixon Souza <maicon_souza09@hotmail.com>
2024-10-08 07:37:50 -0300

    08-10-24 07:37

--- 06_carros/cars/views.py
+++ 06_carros/cars/views.py
@@ -1,34 +1,72 @@
 from django.shortcuts import render, redirect
 from .models import Car
 from .form import CarModelForm
+from django.views import View
 
 
-def cars_view(request):
-    cars = {}
-    search = request.GET.get('search')
+# def cars_view(request):
+#     cars = {}
+#     search = request.GET.get('search')
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search).order_by('model')
-    else:
-        cars = Car.objects.all().order_by('model')
+#     if search:
+#         cars = Car.objects.filter(model__icontains=search).order_by('model')
+#     else:
+#         cars = Car.objects.all().order_by('model')
 
-    context = {
-        "cars": cars
-    }
-    return render(request, 'cars.html', context)
+#     context = {
+#         "cars": cars
+#     }
+#     return render(request, 'cars.html', context)
+
+class CarsView(View):
+    def get(self, request):
+        cars = {}
+        search = request.GET.get('search')
+
+        if search:
+            cars = Car.objects.filter(
+                model__icontains=search).order_by('model')
+        else:
+            cars = Car.objects.all().order_by('model')
+
+        context = {
+            "cars": cars
+        }
+        return render(request, 'cars.html', context)
 
 
-def new_car_view(requets):
-    new_car_form = CarModelForm()
-    if requets.method == 'POST':
-        new_car_form = CarModelForm(requets.POST, requets.FILES)
+# def new_car_view(requets):
+#     new_car_form = CarModelForm()
+#     if requets.method == 'POST':
+#         new_car_form = CarModelForm(requets.POST, requets.FILES)
 
-        if new_car_form.is_valid():
-            new_car_form.save()
+#         if new_car_form.is_valid():
+#             new_car_form.save()
+#             return redirect('cars_list')
+
+#     context = {
+#         "new_car_form": new_car_form
+#     }
+
+#     return render(requets, 'new_car.html', context)
+
+
+class NewCarView(View):
+    def get(self, request):
+        new_car_from = CarModelForm()
+        context = {
+            "new_car_form": new_car_from
+        }
+        return render(request, 'new_car.html', context)
+
+    def post(self, request):
+        new_car_from = CarModelForm(request.POST, request.FILES)
+        if new_car_from.is_valid():
+            new_car_from.save()
             return redirect('cars_list')
-
-    context = {
-        "new_car_form": new_car_form
-    }
-
-    return render(requets, 'new_car.html', context)
+        else:
+            new_car_form = CarModelForm()
+            context = {
+                "new_car_form": new_car_form
+            }
+            return render(request, 'new_car.html', context)
