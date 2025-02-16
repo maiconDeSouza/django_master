7676ea1fbb634357aea53142d6aa22565d211306
Maicon Souza <maicon_souza09@hotmail.com>
2025-02-15 22:34:06 -0300

    create first model

--- 06_cars/cars/models.py
+++ 06_cars/cars/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField()
+    value = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.model} - {self.brand}'
