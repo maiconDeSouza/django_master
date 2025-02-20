b946768579eb914da0dd5770c4a892312be99ca5
Maicon Souza <maicon_souza09@hotmail.com>
2025-02-19 22:04:07 -0300

    create form - django

--- 06_cars/cars/forms.py
+++ 06_cars/cars/forms.py
@@ -1,14 +1,6 @@
 from django import forms
 from .models import Brand
 
-# model = models.CharField(max_length=200)
-#     brand = models.ForeignKey(
-#         Brand, on_delete=models.PROTECT, related_name='car_brand')
-#     factory_year = models.IntegerField()
-#     plate = models.CharField(max_length=10, blank=True, null=True)
-#     value = models.FloatField(blank=True, null=True)
-#     photo = models.ImageField(upload_to='cars/', blank=True, null=True)
-
 
 class CarsForms(forms.Form):
     model = forms.CharField(max_length=200)
